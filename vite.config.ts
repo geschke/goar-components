76138d9e562806527e4aa8734365c222ef38d746
Ralf Geschke <ralf@kuerbis.org>
2024-08-07 19:41:47 +0200

    backup commit before rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue';
 //import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts'; // Für Typendefinitionen
+//import commonjs from '@rollup/plugin-commonjs';
+
+//import { cjsInterop } from "vite-plugin-cjs-interop";
+ 
 
 export default defineConfig({
   plugins: [
@@ -14,7 +18,23 @@ export default defineConfig({
         }
       }
     }),
-    dts()
+    dts(),
+    //commonjs(),
+  /*  cjsInterop({
+      // Add broken npm package here
+      dependencies: [
+        // Apply patch to root import:
+        //   import someImport from 'some-package'
+        "bootstrap",
+ 
+        // Apply patch to all sub imports:
+        //   import someImport from 'some-package/path'
+        //   import someImport from 'some-package/sub/path'
+        //   ...
+        //"bootstrap/**",
+      ]
+    })
+*/
   ],
   /*resolve: {
     alias: {
@@ -27,17 +47,18 @@ export default defineConfig({
     }
   },
   build: {
+    //just tested... transpile: ['bootstrap'],
     lib: {
       entry: 'src/index.ts',
-      formats: ['es', 'cjs'], // ES-Modul und UMD-Format erstellen war: umd
-      name: 'goar-components', // Name der Komponentensammlung
+      formats: ['es','cjs'], // create es format only, was: cjs and umd formats, was: umd
+      name: 'goar-components', 
       fileName: (format) => `goar-components.${format}.js`,
     },
     rollupOptions: {
-      external: ['vue','pinia','bootstrap'], // Externes Modul
+      external: ['vue','pinia','bootstrap'], // External modules
       output: {
         globals: {
-          vue: 'Vue', // Globale Variable für Vue
+          vue: 'Vue', // Global variable for vue
           pinia: 'Pinia'
         },
       },
